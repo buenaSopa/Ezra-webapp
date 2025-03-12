9fd2bb2264742ccb4a079fb141777de76522f2dd
bryan <bryankoh17102@gmail.com>
2025-03-12 10:47:33 +0800

    project layout with auth

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "../app/utils/supabase/client";
-import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -10,27 +11,17 @@ export default async function AuthButton() {
 
   const signOut = async () => {
     "use server";
-
     const supabase = createClient();
     await supabase.auth.signOut();
     return redirect("/");
   };
 
   return user ? (
-    <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-          Logout
-        </button>
-      </form>
-    </div>
-  ) : (
-    <Link
-      href="/"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-    >
-      Login
-    </Link>
-  );
+    <form action={signOut}>
+      <Button variant="ghost" className="w-full justify-start text-destructive hover:text-destructive">
+        <LogOut className="mr-2 h-4 w-4" />
+        Logout
+      </Button>
+    </form>
+  ) : null;
 }
