6cb4ea54484d8186295dbccd08f64beb2d9a8849
Bryan <bryankoh17102@gmail.com>
2025-03-15 18:28:00 +0800

    refactor supabase folder to drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: '.env.local' });
 
 export default {
   schema: "./schema/*",
-  out: "./supabase/migrations",
+  out: "./drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: `postgresql://postgres.urgmbhtxmdsfqplqybon:${process.env.DATABASE_PASSWORD}@aws-0-eu-west-2.pooler.supabase.com:6543/postgres`
