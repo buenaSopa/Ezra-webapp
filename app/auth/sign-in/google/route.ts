a3875b98de29ce0f41cff08be65a0d90a6dd0365
Bryan <bryankoh17102@gmail.com>
2025-03-15 18:55:26 +0800

    google oauth but no auto profile creation

---
+++ app/auth/sign-in/google/route.ts
@@ -0,0 +1,35 @@
+import { createClient } from "@/app/utils/supabase/server";
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+
+export async function POST() {
+  const headersList = headers();
+  const origin = headersList.get("origin");
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: `${origin}/auth/callback`,
+      queryParams: {
+        access_type: "offline",
+        prompt: "consent",
+      },
+    },
+  });
+
+  if (error) {
+    return NextResponse.redirect(
+      `${origin}/?message=Could not authenticate user`,
+      {
+        // a 301 status is required to redirect from a POST to a GET route
+        status: 301,
+      }
+    );
+  }
+
+  return NextResponse.redirect(data.url, {
+    // a 301 status is required to redirect from a POST to a GET route
+    status: 301,
+  });
+} 
