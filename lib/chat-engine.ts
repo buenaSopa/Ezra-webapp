bc6e000cadae3750f412163a3de62893c06ee17c
Bryan <bryankoh17102@gmail.com>
2025-03-28 15:24:31 +0800

    chat engine

---
+++ lib/chat-engine.ts
@@ -0,0 +1,24 @@
+// lib/chat-engine.ts
+import { ContextChatEngine, LLM, storageContextFromDefaults, VectorStoreIndex } from "llamaindex";
+import { getQdrantVectorStore } from "./qdrant";
+
+export async function createChatEngine(llm: LLM, productId?: string) {
+
+	// Get Qdrant vector store
+	const qdrantVectorStore = getQdrantVectorStore();
+
+	const ctx = await storageContextFromDefaults({ vectorStore: qdrantVectorStore });
+
+	// Create index from the vector store
+	const index = await VectorStoreIndex.init({ storageContext: ctx });
+
+	// Create retriever
+	const retriever = index.asRetriever({
+		similarityTopK: 40
+	});
+
+	// Create and return the chat engine
+	return new ContextChatEngine({
+		retriever,
+	});
+}
